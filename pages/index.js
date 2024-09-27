c7e83881cbf0c3c7b6e5c9559c6a76fa1cdf08ee
Karyn <99372279+karynla@users.noreply.github.com>
2024-09-27 15:55:46 +0000

    atualizacao desafio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>novo commit</h1>
+    return 
+    <div>
+        <h1>Te amo mo</h1>
+
+    
+        <p>Resposta:</p>
+        <img src="/images/desafio.dev.png" alt="resposta" />
+ </div>
 }
 
 export default Home;
