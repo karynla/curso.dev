07854ac083acff263824fd41352e48c91d6a47eb
Karyn <99372279+karynla@users.noreply.github.com>
2025-01-21 20:07:07 +0000

    testes exemplos

---
+++ tests/calculadora.test.js
@@ -0,0 +1,16 @@
+/*
+
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  // espera que o resultado da const (funcao do arquivo calculado da pasta models) seja 4
+  expect(resultado).toBe(4);
+});
+
+test("somar 'banana' + 100 deve retornar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+});
+
+*/
---
+++ tests/exemplos-test.js
@@ -0,0 +1,27 @@
+// test(parametro1, parametro2)
+// test("nome do teste", callbackFunction)
+// test("string", funcao com a rotina de testes)
+
+/*
+- exemplo com uma function direto dentro de test()
+test("nome do teste", function(){
+  console.log("e assim, funciona?")
+});
+*/
+
+/*
+- exemplo com uma arrow function dentro de test()
+test("nome do teste", () => {
+  console.log("e agora?");
+});
+
+test("testando outro teste", () => {
+  console.log("outro teste");
+});
+*/
+
+/*
+test("espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
+*/
