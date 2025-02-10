60a2018c8a96b5fe0ed8e34e821e9a6096b39dcf
Karyn <99372279+karynla@users.noreply.github.com>
2025-02-10 19:09:32 +0000

    atualização BD ssl

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
@@ -25,7 +25,7 @@ async function query(queryObject) {
     console.error(error);
     throw error;
   } finally {
-    // await client.end();
+    await client.end();
   }
 }
 
