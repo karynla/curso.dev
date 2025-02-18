4b3fbc631e5b9f1a6aefa61a25aaf574c6305c46
Karyn <99372279+karynla@users.noreply.github.com>
2025-02-18 21:12:06 +0000

    adds /api/v1/migrations endpoint

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,21 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
-  // espera que o responseBody sera um Array
-  // seja true (se for true o teste passa), toBe() só aceita true ou false
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
+
+/* expect(Array.isArray(responseBody)).toBe(true); espera que o responseBody sera um Array
+  seja true (se for true o teste passa), toBe() só aceita true ou false
+  */
